fwrethen/joomla_template_fwrethen

commit 159218a357dd23bdbb87683ca42ce882f1211f9c

move lightbox to template root, add script to enable it for every <a> linking directly to .jpg|.png|.gif

html/com_einsatzkomponente/einsatzbericht/default.php
@@ -12,11 +12,6 @@ defined('_JEXEC') or die;
 $lang = JFactory::getLanguage();
 $lang->load('com_einsatzkomponente', JPATH_ADMINISTRATOR);
 
-$template_dir = JURI::base() . 'templates/' . JFactory::getApplication()->getTemplate('template')->template;
-JHtml::_('stylesheet', $template_dir.'/css/lightbox.css');
-JHtml::_('jquery.framework', false);
-JHtml::_('script', $template_dir.'/js/lightbox.min.js');
-
 $images = array();
 $thumbs = array();
 $titlepic = false;

html/com_reports2/show/default.php
@@ -12,9 +12,6 @@ $gmaplink = JRequest::getVar('gmaplink', 0);
 if ($gmaplink == '1')
 { $task = 'view=gmap';}
 JHTML::_('stylesheet','report.css',$path.'templates/'.$template.'/css/');
-JHTML::_('stylesheet', 'lightbox.css',$path.'templates/'.$template.'/css/');
-JHTML::_('script', 'jquery-1.11.0.min.js',$path.'templates/'.$template.'/js/');
-JHTML::_('script', 'lightbox.min.js',$path.'templates/'.$template.'/js/');
 ?>
 
 

index.php
@@ -13,6 +13,12 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 //* The following line loads the MooTools JavaScript Library */
 //JHtml::_('behavior.framework', true);
+JHtml::_('jquery.framework', false);
+
+$doc = JFactory::getDocument();
+$doc->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/lightbox.css', $type = 'text/css');
+$doc->addScript($this->baseurl . '/templates/' . $this->template . '/js/lightbox.min.js', 'text/javascript');
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
@@ -22,6 +28,12 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template.css" type="text/css" />
+
+<script>
+jQuery(document).ready(function() {
+	jQuery('a[href*=".png"], a[href*=".gif"], a[href*=".jpg"]').not('[rel*="lightbox"]').attr('rel', 'lightbox');
+});
+</script>
 
 <!--[if lt IE 9]>
     <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>

commit 5bf580a8d9a76ea78c868ad714045047ffcbcac8

com_einsatzkomponente: fix $template_dir not being declared regression introduced in commit 159218a357dd23bdbb87683ca42ce882f1211f9c

html/com_einsatzkomponente/einsatzbericht/default.php
@@ -12,6 +12,8 @@ defined('_JEXEC') or die;
 $lang = JFactory::getLanguage();
 $lang->load('com_einsatzkomponente', JPATH_ADMINISTRATOR);
 
+$template_dir = JURI::base() . 'templates/' . JFactory::getApplication()->getTemplate('template')->template;
+
 $images = array();
 $thumbs = array();
 $titlepic = false;
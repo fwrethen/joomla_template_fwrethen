fwrethen/joomla_template_fwrethen

commit 8b9883a52a1c6de894ed88cddd5b1a6418a881a2

com_einsatzkomponente: add custom year selection

html/com_einsatzkomponente/einsatzberichte/default.php
@@ -66,10 +66,48 @@ $template_dir = JURI::base() . 'templates/' . JFactory::getApplication()->getTem
 
 
 if (!$this->params->get('anzeigejahr','0') and $this->params->get('display_filter_jahre','1')) :
-	$years[] = JHTML::_('select.option', '9999', JTEXT::_('- Alle Einsätze anzeigen -')  , 'id', 'title');
-	$years = array_merge($years, (array)$this->years);
+	$years = array();
+	foreach ($this->years as $year)
+		array_push($years, $year->id);
+	$urlEinsatzberichte = 'index.php?option=com_einsatzkomponente&view=einsatzberichte&list=1';
 
-	echo JHTML::_('select.genericlist',  $years, 'year', ' onchange=submit(); ', 'id', 'title', $this->selectedYear);?>
+	/* Reference: JHtmlDropdown (libraries/cms/html/dropdown.php) */
+	echo JHTML::_('dropdown.init'); ?>
+	<div class="btn-group" style="<?php if (!in_array($this->selectedYear - 1, $years)) echo 'visibility:hidden;'; ?>">
+		<button name="year" value="<?php echo $this->selectedYear - 1; ?>" class="btn btn-large">
+			<a href="<?php echo $urlEinsatzberichte; ?>">
+				<i class="icon-backward"></i>
+			</a>
+		</button>
+	</div>
+	<div class="btn-group">
+		<a href="#" data-toggle="dropdown" class="btn btn-large dropdown-toggle" style="min-width:122px;">
+			<i class=" icon-calendar"></i>
+			<b style="margin:4px;"><?php echo ($this->selectedYear == 9999) ? 'alle' : $this->selectedYear; ?></b>
+			<i class="icon-chevron-down"></i>
+		</a>
+		<ul class="dropdown-menu">
+			<li class="text-center">
+				<a href="<?php echo $urlEinsatzberichte; ?>&year=9999">
+					Alle Einsätze
+				</a>
+			</li>
+			<?php foreach ($years as $year): ?>
+				<li class="text-center">
+					<a href="<?php echo $urlEinsatzberichte . '&year=' . $year; ?>">
+						<?php echo $year; ?>
+					</a>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
+	<div class="btn-group" style="<?php if (!in_array($this->selectedYear + 1, $years)) echo 'visibility:hidden;'; ?>">
+		<button name="year" value="<?php echo $this->selectedYear + 1; ?>" class="btn btn-large">
+			<a href="<?php echo $urlEinsatzberichte; ?>">
+				<i class="icon-forward"></i>
+			</a>
+		</button>
+	</div>
 
     <?php
 endif;

commit b75e2c8d41be802a9f684d8b41306470e7d2d0b6

com_einsatzkomponente: restore compatitbility with mod_einsatz_stats broken in commit 8b9883a

html/com_einsatzkomponente/einsatzberichte/default.php
@@ -336,6 +336,8 @@ if ($this->params->get('display_home_pagination')) :
 </tfoot>
 </table>
 
+<?php /* Hidden input to keep compatibility with mod_einsatz_stats */ ?>
+<input id="year" name="year" value="<?php echo $this->selectedYear; ?>" type="hidden">
 
 <?php if(JFactory::getUser()->authorise('core.create','com_einsatzkomponente.einsatzbericht'.$item->id)): ?><a href="<?php echo JRoute::_('index.php?option=com_einsatzkomponente&view=einsatzberichtform&layout=edit&id=0'); ?>">Einsatz eintragen</a>
 	<?php endif; ?>
39bc6b08fd6415ae33b02231213e9bb4edb106c6
mttronc <mrtn.mtth@gmx.de>
2017-11-03 17:07:24 +0100

    com_mitglieder: add override

---
+++ html/com_mitglieder/abteilung/default.php
@@ -0,0 +1,31 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+JHtml::_('bootstrap.loadCss');
+?>
+
+
+<h1><?php echo $this->abteilung->name; ?></h1>
+<p><?php echo $this->abteilung->description; ?></p>
+
+<?php if(is_array($this->abteilung->mitglieder)):
+	$n = 1;
+?>
+
+<table class="table table-condensed table-striped">
+	<tr>
+		<?php foreach($this->abteilung->mitglieder as $mitglied):
+			$name = $mitglied->name . ", " . $mitglied->vorname;
+		?>
+		<td>
+			<a href="index.php?option=com_mitglieder&layout=default&view=mitglied&id=<?php echo $mitglied->id;?>">
+				<?php echo $name; ?>
+			</a>
+		</td>
+		<?php if($n % 3 == 0): ?></tr><tr><?php endif; ?>
+			<?php $n++; ?>
+		<?php endforeach; ?>
+	</tr>
+</table>
+
+<?php endif; ?>
